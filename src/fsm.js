4fae9c3328dce515b1aa9f683fa3aa4b1dd6453b
aven <Nastya_tretyak16@mail.ru>
2017-02-05 20:06:38 +0300

    the first part of implementation FSM class

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,73 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+
+        this.config = config;
+        if (this.config == null ){
+
+            throw  exception('Error');
+        }
+        this.state = this.config.initial ;
+        this.states = this.config.states;
+        this.transitions = this.config.states[this.state].transitions;
+
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+
+      return  this.state;
+
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+
+        for( var property in this.config.states) {
+            if(property == state){
+
+                return  this.state = state;
+        }
+        }
+        throw  exception('Error');
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+
+        for(var value in this.transitions) {
+        if (value == event) {
+            return (
+
+                    (this.state = this.transitions[value]) &&
+                    (this.transitions = this.states[this.state].transitions)
+
+        );
+
+
+        }
+        }
+
+        throw  exception('Error');
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+
+      return  this.state = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,14 +77,46 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+
+        var array = [];
+        if(event ==null){
+            for(var property in this.states){
+                array.push(property);
+            }
+
+            return array;
+        }
+        else {
+            for (var value in this.states) {
+                this.state = value;
+                this.transitions = this.states[this.state].transitions;
+                for(var transit in this.transitions) {
+                    if(transit == event) {
+                        array.push(this.state);
+                }
+                }
+
+            }
+            return array;
+        }
+
+            }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if (this.state == this.config.initial){
+
+            return false;
+        }
+
+
+    }
+
 
     /**
      * Goes redo to state.
